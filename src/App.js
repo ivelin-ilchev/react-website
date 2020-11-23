dd4da5580e40608ced54b835a17faaadb817ba5b
Ivi <ivelinilcev@gmail.com>
2020-11-23 20:36:51 +0200

    revert to default

--- src/App.js
+++ src/App.js
@@ -4,28 +4,19 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header>
-        <div className="main">
-          <div className="logo">
-            <img src="images/logo.jpg" alt="logo" />
-          </div>
-          <ul>
-            <li><a href="#">Home</a></li>
-            <li><a href="services.html">Services</a></li>
-            <li><a href="gallery.html">Gallery</a></li>
-            <li><a href="about.html">About</a></li>
-            <li><a href="https://www.facebook.com/profile.php?id=100000875270810">Contact</a></li>
-            <li><a href="login.html">Login</a></li>
-          </ul>
-        </div>
-        <div className="title">
-          <h1>Website</h1>
-        </div>
-
-        <div className="button">
-          <a href="https://www.youtube.com/" className="btn">Watch video</a>
-          <a href="#" className="btn">Learn more</a>
-        </div>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
       </header>
     </div>
   );
